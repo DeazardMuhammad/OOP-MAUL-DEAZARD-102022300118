09a16bcadf0f73f7716c75e40d04a23b451c9a9e
DeazardMuhammad <deazard29@gmail.com>
2024-10-11 13:54:34 +0700

    test

---
+++ TP0.java
@@ -0,0 +1,9 @@
+package TP0;
+// Deazard Muhammad Arrayyan
+// 102022300118
+// SI4701
+public class TP0 {
+    public static void main(String[] args) {
+        System.out.println("Goodbye, World");
+    }
+}
