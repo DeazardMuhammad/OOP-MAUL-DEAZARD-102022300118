615ef3b0436900c9344c86af796bfc88382bfa73
DeazardMuhammad <deazard29@gmail.com>
2024-10-11 14:30:28 +0700

    TP0

--- TPMODUL0_DEAZARD/TPMODUL0_DEAZARD.java
+++ TPMODUL0_DEAZARD/TPMODUL0_DEAZARD.java
@@ -1,5 +1,5 @@
 public class TPMODUL0_DEAZARD {
     public static void main(String[] args) {
-        System.out.println("Goodbye, World!!");
+        System.out.println("Goodbyeee, World!!!!!");
     }
 }
